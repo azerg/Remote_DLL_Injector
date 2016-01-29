0d7e9169757cac60bc2ba2a5e41887a7996b0fce
azerg <pub@azerg.com>
2016-01-29 17:45:03 +0200

    console now supports params :dancers:
    instead of dummy hardcode

--- Remote_DLL_Injector/build.bat
+++ Remote_DLL_Injector/build.bat
@@ -1,2 +1,2 @@
 REM rd /s /q build
-cmake . -B"build" -G"Visual Studio 14" -Wno-dev -DBOOST_ROOT="F:\Work_Installs\boost_1_58_0" -DBOOST_LIBRARYDIR="F:\Work_Installs\boost_1_58_0\lib" -DBoost_USE_STATIC_LIBS=ON
+cmake . -B"build" -G"Visual Studio 14" -Wno-dev -DBOOST_ROOT="E:\common_libs\boost_1_59_0" -DBOOST_LIBRARYDIR="E:\common_libs\boost_1_59_0\stage\lib" -DBoost_USE_STATIC_LIBS=ON
--- Remote_DLL_Injector/remInjectTest/remInjectTest.cpp
+++ Remote_DLL_Injector/remInjectTest/remInjectTest.cpp
@@ -9,8 +9,60 @@
 #include <iterator>
 #include <stdio.h>
 #include <tchar.h>
-#include <boost/filesystem/path.hpp>
-#include <boost/filesystem/operations.hpp>
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+
+
+struct CmdOptions
+{
+  std::string targetProcessName;
+  std::string dllToInjectPath;
+  bool injectWithLocalDLL; /*true*/
+  std::string localDLLPath;
+};
+
+template <class Type>
+void GetValFromCmd(const po::variables_map& vm, const char* fieldName, Type& outVar)
+{
+  outVar = vm[fieldName].as<Type>();
+}
+
+CmdOptions ParseCmd(int argc, _TCHAR* argv[])
+{
+  CmdOptions result;
+
+  // Declare the supported options.
+  po::options_description desc("Allowed options");
+  desc.add_options()
+  ("help,h", "produce help message")
+  ("target_process_name", po::value<std::string>()->default_value(""), "Target process name (e.g. \"target.exe\")")
+  ("source_dll_path", po::value<std::string>()->default_value(""), "Full path to dll to inject")
+  ("with_local_dll", po::value<bool>()->default_value(false), "Inject with local dll flag (default false)")
+  ("local_dll_path", po::value<std::string>()->default_value(""), "Full path to the local dll that will be created during process of injection")
+  ;
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
+
+  if (vm.count("help"))
+  {
+    std::cout << desc << "\n" << "Sample params str:\n" <<
+      "--target_process_name=\"TargetApp.exe\" "
+      "--source_dll_path=\"D:\\Work\\other\\Remote_DLL_Injector\\Remote_DLL_Injector\\bin\\Debug\\SampleDll.dll\" "
+      "--with_local_dll=yes "
+      "--local_dll_path=\"D:\\Work\\other\\Remote_DLL_Injector\\Remote_DLL_Injector\\bin\\Debug\\dummy_local.dll\"";
+    return CmdOptions{};
+  }
+
+  GetValFromCmd(vm, "target_process_name", result.targetProcessName);
+  GetValFromCmd(vm, "source_dll_path", result.dllToInjectPath);
+  GetValFromCmd(vm, "with_local_dll", result.injectWithLocalDLL);
+  GetValFromCmd(vm, "local_dll_path", result.localDLLPath);
+
+  return result;
+}
 
 std::vector<uint8_t> ReadFileContents(std::string filePath)
 {
@@ -24,30 +76,34 @@ std::vector<uint8_t> ReadFileContents(std::string filePath)
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-  boost::filesystem::path dllToInject{boost::filesystem::current_path()};
-  dllToInject /= "SampleDll.dll";
+  auto cmdOptions = ParseCmd(argc, argv);
+
+  if (cmdOptions.dllToInjectPath.empty())
+  {
+    std::cout << "Type -h for help" << std::endl;
+    return -1;
+  }
 
   // inject
   static StealthParamsIn in;
   static StealthParamsOut out;
   memset(&in, 0, sizeof(in));
   memset(&out, 0, sizeof(out));
-  in.dllToInject = ReadFileContents(dllToInject.generic_string());
-  in.process = "TargetApp.exe";
+  in.dllToInject = ReadFileContents(cmdOptions.dllToInjectPath.c_str());
+  in.process = cmdOptions.targetProcessName;
   in.params = {1,2,3,4,5,6,7,8,9,0,9,8,7,6,5,4,3,2,1}; // sample params arr :D
   in.removeExtraSections = true;
   in.removePEHeader = true;
   in.randomHead = true;
   in.randomTail = true;
   in.randomMax = 1024*5;
-  in.injectWithLocalDll = true;
-  in.localDllPath = (boost::filesystem::current_path() /= "dummyLocal.dll").generic_string();
+  in.injectWithLocalDll = cmdOptions.injectWithLocalDLL;
+  in.localDllPath = cmdOptions.localDLLPath;
 
   StealthInject inj;
   SIError err = inj.Inject(&in, &out);
 
   std::cout << "SIError: " << err << std::endl;
-  system("pause");
 
   return 0;
 }
